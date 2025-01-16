cd5ef61affd0f974485e24ae95de1abc9b005c07
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2025-01-16 16:58:44 +0100

    improvements to enque scripts

--- crawler/src/enqueue-bill.ts
+++ crawler/src/enqueue-bill.ts
@@ -1,17 +1,37 @@
-// import { inngest } from './ingest';
-// import BILLS from '../data/bills-5500.json';
+import { inngest } from './ingest';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-// async function main() {
-//   // This sends an event to Inngest.
-//   const a = await inngest.send(
-//     BILLS.bills.map(bill => ({
-//       name: 'bill.imported',
-//       id: `${bill.congress}-${bill.originChamberCode.toLowerCase()}-${bill.number}`,
-//       data: bill,
-//     })),
-//   );
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
-//   console.log(a);
-// }
+const DATA = path.join(__dirname, '..', 'data');
 
-// main().catch(console.error);
+async function main() {
+  // list all files
+  const files = await fs.readdir(DATA);
+
+  const allDataFiles = files.filter(file => file.startsWith('bill-'));
+
+  // load a file, iterate over contents and send to Inngest
+  for await (const file of allDataFiles) {
+    const data = await fs.readFile(path.join(DATA, file), 'utf-8');
+    const contents = JSON.parse(data);
+
+    console.log(`Sending ${contents.bills.length} bills to Inngest...`);
+
+    const ing = await inngest.send(
+      // @ts-expect-error - gotta type this better
+      contents.bills.map(bill => ({
+        name: 'bill.imported',
+        id: `${bill.congress}-${bill.originChamberCode.toLowerCase()}-${bill.number}`,
+        data: bill,
+      })),
+    );
+
+    console.log(`Sent ${ing.ids.length} bills to Inngest.`);
+  }
+}
+
+main().catch(console.error);
