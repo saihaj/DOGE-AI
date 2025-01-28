8d91797cf7b3c962aafb1adb3db268395f12adff
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2025-01-28 23:24:17 +0000

    limit scope of replies to his own threads

--- agent/src/twitter/process.ts
+++ agent/src/twitter/process.ts
@@ -12,6 +12,42 @@ const openai = createOpenAI({
 });
 
 /**
+ * Finds the parent tweet in a thread. If we hit the limit just give error.
+ */
+async function findParentTweet({ id }: { id: string }) {
+  // Just to limit the number of tweets we fetch
+  // if we go to this time to ignore that tweet for sure LOL
+  const LIMIT = 20;
+  let tweets: Awaited<ReturnType<typeof getTweet>>[] = [];
+
+  let searchId: null | string = id;
+  do {
+    const tweet = await getTweet({ id: searchId });
+    tweets.push(tweet);
+    if (tweet.inReplyToId) {
+      searchId = tweet.inReplyToId;
+    }
+
+    if (tweet.inReplyToId === null) {
+      searchId = null;
+    }
+
+    // Limit max tweets
+    if (tweets.length > LIMIT) {
+      searchId = null;
+    }
+  } while (searchId);
+
+  if (tweets.length > LIMIT) {
+    throw new NonRetriableError(REJECTION_REASON.TOO_DEEP_OF_A_THREAD);
+  }
+
+  const parentTweet = tweets[tweets.length - 1];
+
+  return parentTweet;
+}
+
+/**
  * Fetches tweets from the Twitter API and queues them for processing.
  */
 export const processTweets = inngest.createFunction(
@@ -79,6 +115,27 @@ export const processTweets = inngest.createFunction(
         if (!shouldEngage) {
           throw new NonRetriableError(
             REJECTION_REASON.SPAM_DETECTED_DO_NOT_ENGAGE,
+          );
+        }
+
+        // Ensure that it only replies to his own tweet threads
+        // For any interactions outside his own threads we will ignore
+        const letItReply = await step.run('let-it-reply', async () => {
+          const parentTweet = await findParentTweet({
+            id: event.data.inReplyToId!,
+          });
+
+          // He himself is the author of the thread
+          if (parentTweet.author.id === event.data.inReplyToUserId) {
+            return true;
+          }
+
+          return false;
+        });
+
+        if (!letItReply) {
+          throw new NonRetriableError(
+            REJECTION_REASON.NO_REPLY_FOR_INTERACTION_THREADS,
           );
         }
 
