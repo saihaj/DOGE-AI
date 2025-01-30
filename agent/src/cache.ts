013e5e97a543e1ef6c539161b190e1f6025a21c8
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2025-01-30 16:21:50 +0000

    ci: type checking (#61)
    detect bugs that we can with simple type checking. Useful for when I am
    trying to do deploys...

--- agent/src/cache.ts
+++ agent/src/cache.ts
@@ -1,9 +1,10 @@
 import { BentoCache, bentostore } from 'bentocache';
 import { memoryDriver } from 'bentocache/drivers/memory';
 
-export const bento = new BentoCache({
-  default: 'memstore',
-  stores: {
-    memstore: bentostore().useL1Layer(memoryDriver()),
-  },
-});
+export const bento: BentoCache<{ memstore: ReturnType<typeof bentostore> }> =
+  new BentoCache({
+    default: 'memstore',
+    stores: {
+      memstore: bentostore().useL1Layer(memoryDriver()),
+    },
+  });
