607333eda879df92cc430de738f60628192bc1db
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2025-02-11 00:21:43 +0530

    tagging testing script

---
+++ agent/src/temp/tag-twitter.ts
@@ -0,0 +1,74 @@
+import * as readline from 'node:readline/promises';
+import { getReasonBillContext } from '../twitter/execute-interaction';
+import { CoreMessage } from 'ai';
+import { PROMPTS } from '../twitter/prompts';
+import { generateReply } from '../twitter/execute';
+import { logger } from '../logger';
+import { mergeConsecutiveSameRole } from '../twitter/helpers';
+
+const log = logger.child({ module: 'cli-tag-twitter' });
+
+/**
+ * Testing tags on a tweet where DOGEai gets pinged.
+ */
+async function main() {
+  const terminal = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  try {
+    const question = await terminal.question('Enter your question: ');
+    terminal.close();
+
+    const bill = await getReasonBillContext(
+      {
+        messages: [
+          {
+            role: 'user',
+            content: question,
+          },
+        ],
+      },
+      log,
+    ).catch(_ => {
+      return null;
+    });
+    const summary = bill ? `${bill.title}: \n\n${bill.content}` : '';
+    if (bill) {
+      log.info(
+        {
+          billId: bill.id,
+          billTitle: bill.title,
+        },
+        'found bill',
+      );
+    }
+    const messages: Array<CoreMessage> = [];
+
+    if (summary) {
+      messages.push({
+        role: 'user',
+        content: `Context from database: ${summary}\n\n`,
+      });
+    }
+
+    messages.push({
+      role: 'user',
+      content: PROMPTS.REPLY_TWEET_QUESTION_PROMPT({
+        question: question,
+      }),
+    });
+
+    const mergedMessages = mergeConsecutiveSameRole(messages);
+    log.info(mergedMessages, 'context given');
+
+    const { text } = await generateReply({ messages });
+
+    console.log('\n\nResponse: ', text, '\n\n');
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+}
+
+main().catch(console.error);
