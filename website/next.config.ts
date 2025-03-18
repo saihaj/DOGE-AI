b3141e24822294fceaa13528b067d1a68235e5aa
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2025-03-18 12:28:11 +0530

    feat: redirect for store

--- website/next.config.ts
+++ website/next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/store',
+        destination: 'https://dogeai.printify.me',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
