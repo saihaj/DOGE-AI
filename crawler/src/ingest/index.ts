63747af4629507c48718ffa779df43ea41841947
Silas <silas@silaspath.com>
2025-03-17 19:48:34 -0500

    register scheduled scrape

--- crawler/src/ingest/index.ts
+++ crawler/src/ingest/index.ts
@@ -2,12 +2,14 @@ import { embedBill } from './embed-bill';
 import { processBill } from './process-bill';
 import { processDocument } from './process-document';
 import { processWebpage } from './process-web';
+import { scheduledScrape } from './scheduled-scrape';
 
 export const functions = [
   processBill,
   embedBill,
   processDocument,
   processWebpage,
+  scheduledScrape
 ];
 
 export { inngest } from './client';
