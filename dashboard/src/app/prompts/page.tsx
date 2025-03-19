c8075c1651620c3e5708d1ed6af34f58473f0a51
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2025-03-20 02:28:48 +0530

    sorta there

--- dashboard/src/app/prompts/page.tsx
+++ dashboard/src/app/prompts/page.tsx
@@ -1,45 +1,30 @@
 'use client';
 import { Header } from '@/components/header';
-import { useSidebar } from '@/components/ui/sidebar';
-import Editor from '@monaco-editor/react';
+import { Editor } from '@monaco-editor/react';
 import { useTheme } from 'next-themes';
-import { useEffect, useRef } from 'react';
 
 export default function Prompts() {
   const { theme } = useTheme();
-  const { state } = useSidebar();
-  const editorRef =
-    useRef<
-      Parameters<NonNullable<Parameters<typeof Editor>['0']['onMount']>>['0']
-    >(null);
-
-  const updateEditorLayout = () => {
-    if (editorRef.current) {
-      editorRef.current.layout(); // Force Monaco to re-layout based on container size
-    }
-  };
-
-  useEffect(() => {
-    updateEditorLayout();
-    window.addEventListener('resize', updateEditorLayout);
-    return () => window.removeEventListener('resize', updateEditorLayout);
-  }, [state]);
 
   return (
     <>
       <Header />
       <Editor
-        height="100%"
-        width="100%"
-        defaultLanguage="markdown"
         theme={theme === 'dark' ? 'vs-dark' : 'light'}
-        onMount={editor => {
-          editorRef.current = editor;
-        }}
         options={{
           wordWrap: 'on',
-
+          inDiffEditor: false,
+          smartSelect: {
+            selectSubwords: true,
+            selectLeadingAndTrailingWhitespace: true,
+          },
           fontSize: 18,
+          readOnly: false,
+          mouseWheelZoom: false,
+          selectOnLineNumbers: true,
+          cursorBlinking: 'blink',
+          cursorStyle: 'line',
+          contextmenu: true,
           minimap: {
             enabled: false,
           },
