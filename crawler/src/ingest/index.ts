22105503ac91f696b6685c9d6cbf9616d372ec25
Silas <silas@silaspath.com>
2025-03-20 22:10:29 -0500

    update import

--- crawler/src/ingest/index.ts
+++ crawler/src/ingest/index.ts
@@ -2,14 +2,14 @@ import { embedBill } from './embed-bill';
 import { processBill } from './process-bill';
 import { processDocument } from './process-document';
 import { processWebpage } from './process-web';
-import { scheduledScrape } from './scheduled-scrape';
+import { scheduledDogeWebsiteScrape } from './scheduled-scrape';
 
 export const functions = [
   processBill,
   embedBill,
   processDocument,
   processWebpage,
-  scheduledScrape,
+  scheduledDogeWebsiteScrape,
 ];
 
 export { inngest } from './client';
