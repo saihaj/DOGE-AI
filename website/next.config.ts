6e824616f7eea722d9f6e5305c0a9fde488936bb
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2025-03-29 21:47:27 +0530

    run type check part of monorepo

--- website/next.config.ts
+++ website/next.config.ts
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  typescript: {
+    // we run this in a monorepo, so we need to ignore build errors
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
