89afb9ae5566b2074cd342f70011a7aaffbe8aa1
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2025-03-29 21:53:09 +0530

    oops

--- website/next.config.ts
+++ website/next.config.ts
@@ -12,10 +12,6 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-  typescript: {
-    // we run this in a monorepo, so we need to ignore build errors
-    ignoreBuildErrors: true,
-  },
 };
 
 export default nextConfig;
