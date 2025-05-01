8972a8492d6b66f184c3afecc433437fb6334a7a
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2025-05-02 01:45:58 +0530

    more fixes

--- chat/src/app/page.tsx
+++ chat/src/app/page.tsx
@@ -202,9 +202,9 @@ export default function Home() {
         <main className="h-dvh flex-grow flex-shrink relative selection:bg-highlight w-0 @container isolate">
           <div className="relative flex flex-col items-center h-full @container/main">
             <div className="w-full h-full overflow-y-auto overflow-x-hidden scrollbar-gutter-stable flex flex-col items-center px-5">
-              <header className="w-full">
-                <div className="flex items-center justify-between w-full mt-4">
-                  <div className="flex">
+              <header className="w-full sticky top-0 z-50 bg-background mask-b-from-90% backdrop-blur-md pb-2">
+                <div className="flex items-center justify-between w-full mt-2">
+                  <div className="flex items-center">
                     <Logo height={40} width={40} className="rounded-full" />
                     <span className="text-2xl ml-2 font-bold gradient-america text-transparent bg-clip-text">
                       DOGEai
