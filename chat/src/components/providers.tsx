13d00feab665bd25c1e6a88bcb2f46ea47e9a505
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2025-05-06 00:02:25 +0530

    for now just wallet login

--- chat/src/components/providers.tsx
+++ chat/src/components/providers.tsx
@@ -12,7 +12,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
           showWalletLoginFirst: false,
           walletChainType: 'solana-only',
         },
-        loginMethods: ['wallet', 'twitter'],
+        loginMethods: ['wallet'],
         // @ts-ignore i don't want to deal with the types here right now.
         externalWallets: { solana: { connectors: toSolanaWalletConnectors() } },
       }}
