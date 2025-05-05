48bd14bb8fd362e2bc4d0f03746d13f1cacdb30d
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2025-05-06 00:01:30 +0530

    make wallet login functional

--- chat/next.config.ts
+++ chat/next.config.ts
@@ -29,6 +29,11 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  webpack: config => {
+    config.externals['@solana/web3.js'] = 'commonjs @solana/web3.js';
+    config.externals['@solana/spl-token'] = 'commonjs @solana/spl-token';
+    return config;
+  },
   redirects: async () => {
     return [
       {
--- chat/src/components/providers.tsx
+++ chat/src/components/providers.tsx
@@ -1,11 +1,21 @@
 'use client';
 import { PrivyProvider } from '@privy-io/react-auth';
+import { toSolanaWalletConnectors } from '@privy-io/react-auth/solana';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <PrivyProvider
       appId="cma5o9e5t00dric0opxjxfjuc"
       clientId="client-WY6L6yXLaPvbM94Fo9arL3ncuWyRXL4vaTSJtQMCCH2Mv"
+      config={{
+        appearance: {
+          showWalletLoginFirst: false,
+          walletChainType: 'solana-only',
+        },
+        loginMethods: ['wallet', 'twitter'],
+        // @ts-ignore i don't want to deal with the types here right now.
+        externalWallets: { solana: { connectors: toSolanaWalletConnectors() } },
+      }}
     >
       {children}
     </PrivyProvider>
