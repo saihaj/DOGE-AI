f624730ddcec73e3f15ca563a33fe028a1a5e92e
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2025-05-21 21:32:39 -0400

    bind fetch

--- chat/src/lib/trpc/server.ts
+++ chat/src/lib/trpc/server.ts
@@ -1,13 +1,14 @@
 'use server';
-import { createTRPCClient, httpBatchLink } from '@trpc/client';
+import { createTRPCClient, httpLink } from '@trpc/client';
 import type { AppRouter } from '../../../../agent/src/chat-api/router';
 import { API_URL } from '../const';
 
 // Create a tRPC client for server-side (RSC)
 export const trpcServerClient = createTRPCClient<AppRouter>({
   links: [
-    httpBatchLink({
+    httpLink({
       url: `${API_URL}/api/trpc`,
+      fetch: fetch.bind(globalThis),
     }),
   ],
 });
