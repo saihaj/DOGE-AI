e7631f0904816becf47e137f020d226a08834150
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2025-06-09 14:40:45 -0400

    remove animation

--- website/components/sticky-cards/index.tsx
+++ website/components/sticky-cards/index.tsx
@@ -1,6 +1,4 @@
 import { cn } from '@/lib/utils';
-import { motion, MotionValue, useScroll, useTransform } from 'motion/react';
-import { useRef } from 'react';
 import { IconType } from 'react-icons';
 
 export type CardType = {
@@ -11,54 +9,28 @@ export type CardType = {
 };
 
 export const StickyCards = ({ cards }: { cards: CardType[] }) => {
-  const ref = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ['start start', 'end start'],
-  });
-
   return (
     <>
-      <div ref={ref} className="relative">
+      <div className="relative">
         {cards.map((c, idx) => (
-          <Card
-            key={c.id}
-            card={c}
-            length={cards.length}
-            scrollYProgress={scrollYProgress}
-            position={idx + 1}
-          />
+          <Card key={c.id} card={c} position={idx + 1} />
         ))}
       </div>
     </>
   );
 };
 
-const Card = ({
-  position,
-  card,
-  length,
-  scrollYProgress,
-}: {
-  position: number;
-  card: CardType;
-  length: number;
-  scrollYProgress: MotionValue;
-}) => {
-  const scaleFromPct = (position - 1) / length;
-  const y = useTransform(scrollYProgress, [scaleFromPct, 1], [0, -CARD_HEIGHT]);
-
+const Card = ({ position, card }: { position: number; card: CardType }) => {
   const isOddCard = position % 2;
 
   return (
-    <motion.div
+    <div
       style={{
         height: CARD_HEIGHT,
-        y: position === length ? undefined : y,
         color: isOddCard ? 'white' : 'black',
       }}
       className={cn(
-        'sticky top-0 flex w-full origin-top flex-col items-center justify-center px-4',
+        'flex w-full origin-top flex-col items-center justify-center px-4',
         isOddCard ? 'bg-primary' : 'bg-white',
       )}
     >
@@ -69,7 +41,7 @@ const Card = ({
       <p className="mb-8 max-w-3xl text-center text-md md:text-lg">
         {card.description}
       </p>
-    </motion.div>
+    </div>
   );
 };
 
