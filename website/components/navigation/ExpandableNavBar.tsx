f8bbf701d8334aeda132a66d868687edac27f738
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2025-06-09 10:52:51 -0400

    remove banner

--- website/components/navigation/ExpandableNavBar.tsx
+++ website/components/navigation/ExpandableNavBar.tsx
@@ -5,10 +5,10 @@ import { FlipNavbar } from './FlipNav';
 export const ExpandableNavBar = ({ children }: { children?: ReactNode }) => {
   return (
     <>
-      <div className="bg-theme-blue pt-2">
-        {/* <Announcement /> */}
-        <FlipNavbar />
-      </div>
+      {/* <div className="bg-theme-blue pt-2">
+        <Announcement />
+      </div> */}
+      <FlipNavbar />
       <motion.main layout>
         <div className="bg-white">{children}</div>
       </motion.main>
