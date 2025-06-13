ee1ae9c6b4f13a27a3c5420f31fd169846908b41
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2025-06-13 18:11:16 -0400

    feat: add index for loading tweet chats (#414)

--- database/src/schema.ts
+++ database/src/schema.ts
@@ -11,6 +11,7 @@ import {
 } from 'drizzle-orm/sqlite-core';
 import * as crypto from 'node:crypto';
 import { relations, sql } from 'drizzle-orm';
+import { table } from 'node:console';
 
 const float32Array = customType<{
   data: number[];
@@ -128,36 +129,44 @@ export const prismaMigrations = sqliteTable('_prisma_migrations', {
   appliedStepsCount: integer('applied_steps_count').default(0).notNull(),
 });
 
-export const chat = sqliteTable('Chat', {
-  id: text().primaryKey().notNull(),
-  createdAt: numeric()
-    .default(sql`(CURRENT_TIMESTAMP)`)
-    .notNull(),
-  updatedAt: numeric()
-    .default(sql`(CURRENT_TIMESTAMP)`)
-    .notNull(),
-  user: text()
-    .notNull()
-    .references(() => user.id, { onDelete: 'cascade' }),
-  tweetId: text(),
-});
+export const chat = sqliteTable(
+  'Chat',
+  {
+    id: text().primaryKey().notNull(),
+    createdAt: numeric()
+      .default(sql`(CURRENT_TIMESTAMP)`)
+      .notNull(),
+    updatedAt: numeric()
+      .default(sql`(CURRENT_TIMESTAMP)`)
+      .notNull(),
+    user: text()
+      .notNull()
+      .references(() => user.id, { onDelete: 'cascade' }),
+    tweetId: text(),
+  },
+  table => [index('Chat_tweet_id').on(table.tweetId)],
+);
 
-export const message = sqliteTable('Message', {
-  id: text().primaryKey().notNull(),
-  createdAt: numeric()
-    .default(sql`(CURRENT_TIMESTAMP)`)
-    .notNull(),
-  updatedAt: numeric()
-    .default(sql`(CURRENT_TIMESTAMP)`)
-    .notNull(),
-  role: text().notNull(),
-  chat: text()
-    .notNull()
-    .references(() => chat.id, { onDelete: 'cascade' }),
-  text: text().notNull(),
-  tweetId: text(),
-  meta: blob(),
-});
+export const message = sqliteTable(
+  'Message',
+  {
+    id: text().primaryKey().notNull(),
+    createdAt: numeric()
+      .default(sql`(CURRENT_TIMESTAMP)`)
+      .notNull(),
+    updatedAt: numeric()
+      .default(sql`(CURRENT_TIMESTAMP)`)
+      .notNull(),
+    role: text().notNull(),
+    chat: text()
+      .notNull()
+      .references(() => chat.id, { onDelete: 'cascade' }),
+    text: text().notNull(),
+    tweetId: text(),
+    meta: blob(),
+  },
+  table => [index('Message_chat_id').on(table.chat)],
+);
 
 export const messageVector = sqliteTable('MessageVector', {
   id: text().primaryKey().notNull(),
