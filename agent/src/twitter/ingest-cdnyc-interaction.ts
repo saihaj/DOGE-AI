5af5211819dcbcc37d4902a1699059382257088b
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2025-07-15 17:37:07 -0400

    update search query

--- agent/src/twitter/ingest-cdnyc-interaction.ts
+++ agent/src/twitter/ingest-cdnyc-interaction.ts
@@ -36,8 +36,7 @@ export const ingestCdnycInteractionTweets = inngest.createFunction(
      *
      * I tag bot a lot in updates and most of these are useless interactions for him to process so ignoring as much.
      */
-    const searchQuery = `("Zohran Mamdani" OR "Eric Adams") -filter:replies lang:en within_time:7m`;
-
+    const searchQuery = `("Zohran Mamdani" OR "Zohran" OR "Eric Adams" OR "Cuomo") -filter:replies lang:en within_time:7m`;
     const [list, search] = await Promise.all([
       // https://x.com/i/lists/1942682760713298237
       fetchTweetsFromList({
